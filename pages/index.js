e4c227231f3eaeb60d940e97ee058e583abb1506
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-19 21:26:48 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Link from 'next/link';
+import {useRouter} from 'next/router'
 
 function Home() {
+  const router = useRouter();
+  const handleCLick = () => {
+    console.log('Your order was placed successfully!')
+    // router.push('/product')
+    // router.replace('/product')
+  }
   return (
     <>
       <h1>Home page</h1>
       <Link href="/blog"><a>Blog</a></Link>
       <Link href="/product"><a>Product</a></Link>
+      <button onClick={handleCLick}>Place order</button>
     </>
   );
 }
