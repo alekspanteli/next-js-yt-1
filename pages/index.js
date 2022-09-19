6f57aef690991e3ba88d555399207ba0e61f1496
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-19 20:54:08 +0300

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
-function Home(){
-  return <h1>Home page</h1>
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link href="/blog"><a>Blog</a></Link>
+    </>
+  );
 }
 
 export default Home;
